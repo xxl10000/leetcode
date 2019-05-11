0ca3fc2c4299647d807d1ba895e23234a9c78725
xxl10000 <3170101301@zju.edu.cn>
2019-05-11 13:25:45 +0800

    a try!

---
+++ ProjectTryGit/Source.cpp
@@ -0,0 +1,15 @@
+
+/* Brief: This code is uesd to test the connection of github to visual studio.
+** Author: Jason
+** Date: 2018-6-29
+*/
+#include<iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello ,GitHub.com" << endl;
+	cin.get();
+	cout << "dev Git Try" << endl;
+	cout << "what happened?" << endl;
+	return 0;
+}
